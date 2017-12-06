8d888f913a6cbc218bc5fb5a418fcc4338ada4d9
yasuo kurihara <breitling1216@gmail.com>
2017-12-07 07:53:26 +0900

    update view file & style

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.delete_all
+#
+Product.create(title: 'Programming Ruby 2.3',
+  discription:
+    %{
+      <p>
+       Ruby is the fastest growing and most exciting dynamic language
+       out there. if you need to get working programs delivered fast,
+       you should add Ruby to your toolbox.
+      </p>
+    },
+  image_url: 'ruby.jpg',
+  price: 49.95  
+)
